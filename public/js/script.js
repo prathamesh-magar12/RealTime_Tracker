9be263786bcebb446310048420ac3a957e121e96
prathamesh-magar12 <prathamesh.magar12@gmail.com>
2024-12-20 22:23:24 +0530

    script js updated

--- public/js/script.js
+++ public/js/script.js
@@ -17,7 +17,7 @@ if(navigator.geolocation){
     )
 }
 
-const map=L.map("map").setView([0,0], 30);
+const map=L.map("map").setView([0,0], 20);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
     attribution: "OpenStreetMap",
