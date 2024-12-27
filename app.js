9fa05351ea93d18262b3175218175cff84582923
prathamesh-magar12 <prathamesh.magar12@gmail.com>
2024-12-27 23:57:11 +0530

    script.js updated

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 //"A Real-Time Tracker application built with Node.js
-//providing real-time updates and seamless tracking of data. 
-//Includes a user-friendly interface and efficient backend integration for smooth performance."
+// providing real-time updates and seamless tracking of data.
+// Includes a user-friendly interface and efficient backend integration for smooth performance."
 
-const express=require('express');
-const app=express();
-const path=require('path');
+const express = require("express");
+const app = express();
+const path = require("path");
 
-const http=require("http");
+const http = require("http");
 
-const socketio =require("socket.io");
-const server=http.createServer(app);
-const io= socketio(server);
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", function(socket){
-    socket.on ("send-location", function(data){
-        io.emit("receive-location",{id: socket.id, ...data})
-    });
-    socket.on("disconnect", function(){
-        io.emit("user-disconnected", socket.id)
-    });
+io.on("connection", function (socket) {
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+  });
 });
 
-app.get("/", function(req, res){
-    res.render("index");
-})
-
+app.get("/", function (req, res) {
+  res.render("index");
+});
 
 server.listen(3000);
