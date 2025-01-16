78ee126bdd9516a07690dd5d447a2f2c693a13ec
prathamesh-magar12 <prathamesh.magar12@gmail.com>
2025-01-16 22:45:16 +0530

    script.js updated

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", function (socket) {
-  // socket.on("send-location", function (data) {
-  //   io.emit("receive-location", { id: socket.id, ...data });
-  // });
-  // socket.on("disconnect", function () {
-  //   io.emit("user-disconnected", socket.id);
-  // });
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+  });
   console.log("connecteed");
 });
 
@@ -29,4 +29,4 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 
-server.listen(3001);
+server.listen(3000);
--- public/js/script.js
+++ public/js/script.js
@@ -2,44 +2,43 @@ const socket = io();
 
 console.log("hellow brother");
 
-// if(navigator.geolocation){
-//     navigator.geolocation.watchPosition(
-//         (position)=>{
-//             const {latitude,longitude} = position.coords;
-//             socket.emit("send-location", {latitude, longitude});
-//         },
-//         (error)=>{
-//             console.error(error);
-//         },
-//         {
-//             enableHighAccuracy:true,
-//             timeout: 5000,
-//             maximumAge: 0
-//         }
-//     )
-// }
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit("send-location", { latitude, longitude });
+    },
+    (error) => {
+      console.error(error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    }
+  );
+}
 
-// const map=L.map("map").setView([0,0], 20);
+const map = L.map("map").setView([0, 0], 20);
 
-// L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-//     attribution: "OpenStreetMap",
-// }).addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "OpenStreetMap",
+}).addTo(map);
 
-// const markers={};
+const markers = {};
 
-// socket.on("receive-location", (data)=>{
-//     const{id, latitude, longitude} =data;
-//     map.setView([latitude, longitude]);
-//     if(markers[id]){
-//         markers[id].setLatLng([latitude, longitude]);
-//     }
-//     else{
-//         markers[id]= L.marker([latitude, longitude]).addTo(map);
-//     }
-// });
+socket.on("receive-location", (data) => {
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude]);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+});
 
-// socket.on("user-disconnected", (id) =>{
-//     if(markers[id]){
-//         map.removeLayer(markers[id]);
-//     }
-// })
+socket.on("user-disconnected", (id) => {
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+  }
+});
