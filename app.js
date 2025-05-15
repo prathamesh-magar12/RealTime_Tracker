442e616183cef9941b70eb001b3251c5b1c3cf6c
prathamesh-magar12 <prathamesh.magar12@gmail.com>
2025-05-15 23:38:43 +0530

    Fix comment formatting in app.js for consistency

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //"A Real-Time Tracker application built with Node.js
-// providing real-time updates and seamless tracking of data.
+//providing real-time updates and seamless tracking of data.
 // includes a user-friendly interface and efficient backend integration for smooth performance."
 
 const express = require("express");
